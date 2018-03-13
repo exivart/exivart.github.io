dfff81ac2e5711ead37907872d2dbeadc84c9ade
Artem <artem.dunk@ynadex.ru>
2018-03-13 09:24:54 +0300

    commit
    Fixed scrolling to work items

--- js/works.js
+++ js/works.js
@@ -3,22 +3,32 @@ $(document).ready(function() {
   var thumbItem = $('.work--item'),
       worksBelt = $('.works--inner'),
       workDescription = $('.work--description'),
-      descriptionOffset = $('.works--container').offset().top;
+      backBtn = $('.works--back'),
+      descriptionOffset = $('.works--container').offset().top,
+      clickedOffset;
   thumbItem.click(function() {
-   $('html, body').animate({
-                    scrollTop: descriptionOffset - 100
-                    }, 1000);
+   $('html, body').animate({scrollTop: descriptionOffset - 100}, 1000);
     worksBelt.css('margin-left', '-100%');
     workDescription.show();
     var folder = $(this).data('folder'),
         title = $(this).data('title'),
         html = 'include/works/' + folder + '.html',
         loader = '<div class="loader">Loading...</div>';
+    clickedOffset = $(this).offset().top;
     $('#title').text(title);
     $('.description--main').html(loader).load(html);
   });
-  $('#worksBack').click(function() {
+  backBtn.click(function() {
     worksBelt.css('margin-left', 0);
+    if($(window).width() > 768) {
+      $('html, body').animate({
+                       scrollTop: descriptionOffset - 100
+                     }, 500);
+    } else {
+      $('html, body').animate({
+                       scrollTop: clickedOffset - 100
+                     }, 500);
+    }
     workDescription.hide(400);
   });
 });
